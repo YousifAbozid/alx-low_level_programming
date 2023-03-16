ae926e89e0f363e4116b8f8ccc78f307696db82f
Yousif Abozid <yousif.abozid@yahoo.com>
2023-03-17 00:38:12 +0200

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-/*
-* This is the main function works as entry point
-* and always returns 0 for successful exit
-*/
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return(0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }

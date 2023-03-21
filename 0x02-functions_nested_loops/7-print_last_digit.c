b38f534c69c13d50bba15ffe385598ab9b1ad654
Yousif Abozid <yousif.abozid@yahoo.com>
2023-03-22 01:53:29 +0200

    here are only 3 colors, 10 digits, and 7 notes; it's what we do with them that's important

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_last_digit - Entry point
+ * @n: character to classify.
+ * Return: success
+ *
+ */
+
+int print_last_digit(int n)
+{
+return (n % 10);
+}

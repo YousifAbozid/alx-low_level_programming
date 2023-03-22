9da0326e91837a05b4d2a35bdc8019ddaa112f8c
Yousif Abozid <yousif.abozid@yahoo.com>
2023-03-22 04:32:38 +0200

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,22 +1,64 @@
-#include"main.h"
+#include "main.h"
 #include <stdio.h>
-/**
- * print_to_98 - print n to 98 counts
- *
- * @n: input
- *
- * Return void
-*/
 
+/**
+ * numbers_above_98 - Prints numbers to 98
+ * @n: character to classify.
+ *
+ * Description: Print numbers above 98
+ */
+void numbers_above_98(int n)
+{
+int i;
+for (i = n; i >= 98; i--)
+{
+if (i < 10)
+{
+_putchar(i + '0');
+}
+else
+{
+_putchar(i / 10 + '0');
+_putchar(i % 10 + '0');
+}
+if (i != 98)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+}
+
+/**
+ * print_to_98 - Prints numbers to 98
+ * @n: character to classify.
+ */
 void print_to_98(int n)
 {
-	int count;
-
-	if (n > 98)
-		for (count = n; count > 98; --count)
-			printf("%d, ", count);
-	else
-		for (count = n; count < 98; ++count)
-			printf("%d, ", count);
-	printf("98\n");
+int i;
+if (n <= 98)
+{
+for (i = n; i <= 98; i++)
+{
+if (i < 10)
+{
+_putchar(i + '0');
+}
+else
+{
+_putchar(i / 10 + '0');
+_putchar(i % 10 + '0');
+}
+if (i != 98)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+}
+else
+{
+numbers_above_98(n);
+}
+_putchar('\n');
 }

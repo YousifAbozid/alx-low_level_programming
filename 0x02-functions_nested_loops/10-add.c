19b7accfaa2345ef82d9f745ac8ab7fc49797189
Yousif Abozid <yousif.abozid@yahoo.com>
2023-03-22 03:12:33 +0200

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * add - Entry point
+ *
+ * Return: success
+ *
+ */
+
+int add(i, j)
+{
+int sum = i + j;
+_putchar(sum + '0');
+return (0);
+}

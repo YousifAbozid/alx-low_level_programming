22c131325004496f96b1a92cd4c327cf3c65a74d
Yousif Abozid <yousif.abozid@yahoo.com>
2023-03-28 04:09:50 +0200

    Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts2 - Entry point.
+ * @str: pointer
+ *
+*/
+
+void puts2(char *str)
+{
+int i;
+while (str[i] != '\0')
+{
+_putchar(str[i]);
+i += 2;
+}
+}

0d81507dd6ce62b0185a468d80131d61a640c9c4
Yousif Abozid <yousif.abozid@yahoo.com>
2023-03-31 00:08:13 +0200

    Mozart composed his music not for the elite, but for everybody

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,20 +8,26 @@
 
 char *leet(char *s)
 {
-int i, j;
-char from[] = "aeotlAEOTL";
-char to[] = "43071";
+int a = 0, b = 0, l = 5;
+char r[5] = {'A', 'E', 'O', 'T', 'L'};
+char n[5] = {'4', '3', '0', '7', '1'};
 
-for (i = 0; s[i]; i++)
+while (s[a])
 {
-for (j = 0; from[j]; j++)
+b = 0;
+
+while (b < l)
 {
-if (s[i] == from[j])
+if (s[a] == r[b] || s[a] - 32 == r[b])
 {
-s[i] = to[j];
-break;
+s[a] = n[b];
 }
+
+b++;
 }
+
+a++;
 }
+
 return (s);
 }

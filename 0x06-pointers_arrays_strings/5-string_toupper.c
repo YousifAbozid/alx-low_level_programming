4f68f3f51d49a569da4c76aa0b3b93acc1411b7f
Yousif Abozid <yousif.abozid@yahoo.com>
2023-03-30 16:44:15 +0200

    Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * *string_toupper - Entry point
+ *
+ * Return: string
+*/
+
+char *string_toupper(char *s)
+{
+int i = 0;
+while (s[i] != '\0')
+{
+if (islower(s[i]))
+{
+s[i] = toupper(s[i]);
+}
+i++;
+}
+return s;
+}

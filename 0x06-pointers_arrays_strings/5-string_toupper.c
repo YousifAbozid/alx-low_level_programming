fa085db61bb4ca3c1554ba5f364358128fcf4129
Yousif Abozid <yousif.abozid@yahoo.com>
2023-03-30 16:48:35 +0200

    Always look up

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -3,13 +3,14 @@
 
 /**
  * *string_toupper - Entry point
- *
+ * @s: string
  * Return: string
 */
 
 char *string_toupper(char *s)
 {
 int i = 0;
+
 while (s[i] != '\0')
 {
 if (islower(s[i]))
@@ -18,5 +19,5 @@ s[i] = toupper(s[i]);
 }
 i++;
 }
-return s;
+return (s);
 }

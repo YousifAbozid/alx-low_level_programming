e620e5c0e514e49e47c5665a7ea9f77d58bfa17f
Yousif Abozid <yousif.abozid@yahoo.com>
2023-03-31 02:30:22 +0200

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -11,30 +11,24 @@
 
 char *rot13(char *s)
 {
-int i;
-int len = strlen(s);
-char *result = (char *)malloc(len + 1);
+int a = 0;
 
-for (i = 0; s[i] != '\0'; i++)
+while (s[a])
 {
-if (s[i] >= 'a' && s[i] <= 'z')
+while ((s[a] >= 'a' && s[a] <= 'z') || (s[a] >= 'A' && s[a] <= 'Z'))
 {
-/* Rotate lowercase letters by 13 positions */
-result[i] = ((s[i] - 'a') + 13) % 26 + 'a';
-}
-else if (s[i] >= 'A' && s[i] <= 'Z')
+if ((s[a] > 'm' && s[a] <= 'z') || (s[a] > 'M' && s[a] <= 'Z'))
 {
-/* Rotate uppercase letters by 13 positions */
-result[i] = ((s[i] - 'A') + 13) % 26 + 'A';
+s[a] -= 13;
+break;
 }
-else
-{
-/* Copy non-letter characters as is */
-result[i] = s[i];
-}
-}
-/* Add null terminator to the result string */
-result[i] = '\0';
 
-return (result);
+s[a] += 13;
+break;
+}
+
+a++;
+}
+
+return (s);
 }

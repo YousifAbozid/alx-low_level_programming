6166f21138d717c4cb5906739e72e2b3fe3d1388
Yousif Abozid <yousif.abozid@yahoo.com>
2023-04-04 01:34:43 +0200

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strpbrk - Entry point
+ * @haystack: pointer
+ * @needle: accept
+ * Return: char
+*/
+char *_strstr(char *haystack, char *needle)
+{
+return (strstr(haystack, needle));
+}

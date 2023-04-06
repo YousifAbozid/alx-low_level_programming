f055a7e9dbccf5e1834a8c18f44c62ba97223fe7
Yousif Abozid <yousif.abozid@yahoo.com>
2023-04-06 03:37:28 +0200

    task 7

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * is_palindrome - Entry point.
+ * @s: string.
+ * Return: number.
+*/
+int is_palindrome(char *s)
+{
+int len = strlen(s);
+
+if (len <= 1)
+return (1);
+/* recursive case: check if first and last characters match */
+else if (s[0] == s[len - 1])
+{
+/* create a substring that excludes the first and last characters */
+char *sub_str = s + 1;
+sub_str[len - 2] = '\0';
+
+/* recursive call on the substring */
+return (is_palindrome(sub_str));
+}
+/* not a palindrome */
+else
+return (0);
+}

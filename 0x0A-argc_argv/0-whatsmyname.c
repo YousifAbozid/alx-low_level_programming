c0bcb2003839110a9b80add635d01e3684742a49
Yousif Abozid <yousif.abozid@yahoo.com>
2023-04-11 02:14:36 +0200

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: number of arguments
+ * @argv: command line arguments
+ * Return: 0 on success
+*/
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}

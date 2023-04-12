dca99cd683019fb944a34e7e7beb722bbf68b0bc
Yousif Abozid <yousif.abozid@yahoo.com>
2023-04-12 02:47:59 +0200

    0x0B. C - malloc, free task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,15 +9,18 @@
 */
 char *create_array(unsigned int size, char c)
 {
+unsigned int i;
+char *arr;
+
 if (size == 0)
 return (NULL);
 
-char *arr = malloc(size * sizeof(char));
+arr = malloc(size * sizeof(char));
 
 if (arr == NULL)
 return (NULL);
 
-for (unsigned int i = 0; i < size; i++)
+for (i = 0; i < size; i++)
 {
 arr[i] = c;
 }

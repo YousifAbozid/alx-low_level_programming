cde5c41116f6e0b6163598fa0f586c2437549780
Yousif Abozid <yousif.abozid@yahoo.com>
2023-04-14 01:52:04 +0200

    0x0C-more_malloc_free task 3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -9,15 +9,17 @@
 */
 int *array_range(int min, int max)
 {
+int *arr, i;
+
 if (min > max)
 return (NULL);
 
-int *arr = malloc(sizeof(int) * (max - min + 1));
+arr = malloc(sizeof(int) * (max - min + 1));
 
 if (arr == NULL)
 return (NULL);
 
-for (int i = 0; i <= max - min; i++)
+for (i = 0; i <= max - min; i++)
 {
 arr[i] = min + i;
 }

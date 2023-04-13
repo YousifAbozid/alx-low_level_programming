22821af0417d11dde1f44ec2dc64dee3a6c54577
Yousif Abozid <yousif.abozid@yahoo.com>
2023-04-14 01:02:38 +0200

    0x0C-more_malloc_free task 1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * string_nconcat - Entry point
+ * @n: int
+ * @s1: pointer
+ * @s2: pointer
+ * Return: void
+*/
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+void *ptr = malloc(b);
+if (ptr == NULL)
+exit(98);
+
+return (ptr);
+}

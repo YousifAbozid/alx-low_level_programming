770969839d6b004772f5c551c503218aa577eb29
Yousif Abozid <yousif.abozid@yahoo.com>
2023-04-21 01:58:47 +0200

    add 0x10-variadic_functions task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,4 +1,3 @@
-#include <stdarg.h>
 #include "variadic_functions.h"
 
 /**
@@ -20,6 +19,7 @@ int sum_them_all(const unsigned int n, ...)
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 		sum += va_arg(args, int);
+
 	va_end(args);
 
 	return (sum);

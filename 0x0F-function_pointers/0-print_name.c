c6c851e153d3b9581a0d38380ecaa509f9202913
Yousif Abozid <yousif.abozid@yahoo.com>
2023-04-25 03:52:26 +0200

    add 0x0F-function_pointers task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "function_pointers.h"
+
 /**
- * print_name - prints a name
- * @name: the name to print
- * @f: the function to print with
- * Return: void
+ * print_name - print name
+ * @name: name to print
+ * @f: function to use
+ *
+ * Return: none
  */
+
 void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL && f != NULL)
+	{
 		f(name);
+	}
 }

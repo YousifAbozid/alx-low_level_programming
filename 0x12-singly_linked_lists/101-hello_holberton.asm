f3f292868899fa730bf034ed624af51b5a2b9e0d
Yousif Abozid <yousif.abozid@yahoo.com>
2023-04-29 04:48:43 +0300

    add 0x12-singly_linked_lists task 6

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,18 +1,19 @@
-section .data
-    hello db "Hello, Holberton",10
+extern printf           ; declare that we will use the 'printf' function defined elsewhere
 
-section .text
-    global _start
+section .data          ; declare the data section
+msg:   db "Hello, Holberton", 0  ; define a string of characters with a null terminator and name it 'msg'
+fmt:   db "%s", 10, 0          ; define a format string with a newline character and a null terminator and name it 'fmt'
 
-_start:
-    ; write the string to stdout
-    mov eax, 4          ; system call for write
-    mov ebx, 1          ; file descriptor for stdout
-    mov ecx, hello      ; pointer to the string
-    mov edx, 16         ; length of the string
-    int 0x80            ; call the kernel
+section .text          ; declare the text section
+global main            ; define the global entry point of the program as 'main'
 
-    ; exit with status code 0
-    mov eax, 1          ; system call for exit
-    xor ebx, ebx        ; status code 0
-    int 0x80            ; call the kernel
+main:
+    push rbp            ; save the value of RBP on the stack
+    mov rdi,fmt         ; move the address of the format string 'fmt' to the first parameter register RDI
+    mov rsi,msg         ; move the address of the message string 'msg' to the second parameter register RSI
+    mov rax,0           ; move 0 to RAX
+    call printf         ; call the 'printf' function with the values in RDI and RSI as parameters
+
+    pop rbp             ; restore the value of RBP from the stack
+    mov rax,0           ; move 0 to RAX
+    ret                 ; return from the function and return the value in RAX

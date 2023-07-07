ec8fcd205d525e32dd0a9dfc357eef5fcaf049c4
Yousif Abozid <yousif.abozid@yahoo.com>
2023-07-07 09:35:59 +0300

    add 0x14-bit_manipulation task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,11 +9,14 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
+	unsigned long int mask;
+	int bit;
+
 	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);  /* Return -1 if the index is out of range */
 
-	unsigned long int mask = 1UL << index;
-	int bit = (n & mask) ? 1 : 0;
+	mask = 1UL << index;
+	bit = (n & mask) ? 1 : 0;
 
 	return (bit);
 }

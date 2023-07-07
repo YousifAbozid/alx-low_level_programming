dc37dc670fdf1ee2b7944ac8070d0d4f05f98eb3
Yousif Abozid <yousif.abozid@yahoo.com>
2023-07-07 23:48:48 +0300

    add 0x13-more_singly_linked_lists task 13

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,36 +1,72 @@
 #include "lists.h"
 
 /**
- * free_listint_safe - frees a listint_t linked list with loop detection
- * @h: pointer to the head of the list
+ * free_listp2 - frees a linked list
+ * @head: head of a list.
  *
- * Return: The size of the list that was freed
+ * Return: no return.
+ */
+void free_listp2(listp_t **head)
+{
+	listp_t *temp;
+	listp_t *curr;
+
+	if (head != NULL)
+	{
+		curr = *head;
+		while ((temp = curr) != NULL)
+		{
+			curr = curr->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
+}
+
+/**
+ * free_listint_safe - frees a linked list.
+ * @h: head of a list.
+ *
+ * Return: size of the list that was freed.
  */
 size_t free_listint_safe(listint_t **h)
 {
-	size_t list_size = 0;
-	listint_t *current, *temp;
+	size_t nnodes = 0;
+	listp_t *hptr, *new, *add;
+	listint_t *curr;
 
-	if (h == NULL || *h == NULL)
-		return (0);
-
-	current = *h;
-	while (current != NULL)
+	hptr = NULL;
+	while (*h != NULL)
 	{
-		list_size++;
-		if (current < current->next)
+		new = malloc(sizeof(listp_t));
+
+		if (new == NULL)
+			exit(98);
+
+		new->p = (void *)*h;
+		new->next = hptr;
+		hptr = new;
+
+		add = hptr;
+
+		while (add->next != NULL)
 		{
-			temp = current;
-			current = current->next;
-			free(temp);
+			add = add->next;
+			if (*h == add->p)
+			{
+				*h = NULL;
+				free_listp2(&hptr);
+				return (nnodes);
+			}
 		}
-		else
-		{
-			free(current);
-			break;
-		}
+
+		curr = *h;
+		*h = (*h)->next;
+		free(curr);
+		nnodes++;
 	}
 
-	*h = NULL; /* Set the head to NULL */
-	return (list_size);
+	*h = NULL;
+	free_listp2(&hptr);
+	return (nnodes);
 }

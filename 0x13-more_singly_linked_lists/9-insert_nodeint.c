4b99c8b58cda8f14576a2ba81e1f71bbaf52c781
Yousif Abozid <yousif.abozid@yahoo.com>
2023-07-07 17:45:59 +0300

    add 0x13-more_singly_linked_lists task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,32 +10,34 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new_node, *temp = *head;
-	unsigned int count = 0;
+	listint_t *new_node;
 
-	if (head == NULL)
+	/* If the head pointer is NULL and the index is greater than 0, */
+	/* it means the list is empty, and the desired index is invalid. */
+	if (*head == NULL && idx > 0)
 		return (NULL);
 
+	/* If the index is 0, it means we need to insert the new node */
+	/* at the beginning. */
 	if (idx == 0)
-		return (add_nodeint_end(head, n));
-
-	while (temp != NULL)
 	{
-		if (count == idx - 1)
-		{
-			new_node = malloc(sizeof(listint_t));
-			if (new_node == NULL)
-				return (NULL);
+		/* Allocate memory for the new node. */
+		new_node = malloc(sizeof(listint_t));
+		if (new_node == NULL)
+			return (NULL);
 
-			new_node->n = n;
-			new_node->next = temp->next;
-			temp->next = new_node;
-			return (new_node);
-		}
+		/* Set the value and link the new node to the current head. */
+		new_node->n = n;
+		new_node->next = *head;
 
-		count++;
-		temp = temp->next;
+		/* Update the head to point to the new node. */
+		*head = new_node;
+
+		/* Return the address of the new node. */
+		return (new_node);
 	}
 
-	return (NULL);
+	/* Recursively call the function on the next node, reducing the index by 1. */
+	return (insert_nodeint_at_index(&(*head)->next, idx - 1, n));
 }
+

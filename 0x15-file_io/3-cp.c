9da5074b4950bf838476a9f8709d989f1f1484d2
Yousif Abozid <yousif.abozid@yahoo.com>
2023-07-10 15:04:21 +0300

    add 0x15-file_io task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,57 +2,6 @@
 #define BUFFER_SIZE 1024
 
 /**
- * main - Copy the content of a file to another file.
- * @argc: The number of command line arguments.
- * @argv: An array of command line argument strings.
- *
- * Return: 0 on success, or the corresponding error code.
- */
-int main(int argc, char *argv[])
-{
-	const char *file_from;
-	const char *file_to;
-	int fd_from;
-	int fd_to;
-	char buffer[BUFFER_SIZE];
-	ssize_t bytes_read, bytes_written;
-
-	if (argc != 3)
-	{
-		print_error_and_exit("Usage: cp file_from file_to\n", NULL, 97);
-	}
-
-	file_from = argv[1];
-	file_to = argv[2];
-
-	fd_from = open_file(file_from, O_RDONLY, 0);
-	fd_to = open_file(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
-
-	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
-	{
-		bytes_written = write(fd_to, buffer, bytes_read);
-		if (bytes_written == -1)
-		{
-			print_error_and_exit("Error: Can't write to %s\n", file_to, 99);
-		}
-	}
-
-	if (bytes_read == -1)
-	{
-		print_error_and_exit("Error: Can't read from file %s\n", file_from, 98);
-	}
-	close_file(fd_from);
-	close_file(fd_to);
-
-	if (chmod(file_to, 0664) == -1)
-	{
-		print_error_and_exit("Error: Can't set permissions for %s\n", file_to, 100);
-	}
-
-	return (0);
-}
-
-/**
  * open_file - Open a file and handle errors.
  * @filename: The name of the file to open.
  * @flags: The flags to use when opening the file.
@@ -97,4 +46,49 @@ void print_error_and_exit(const char *message, const char *arg, int exit_code)
 {
 	dprintf(STDERR_FILENO, message, arg);
 	exit(exit_code);
+}
+
+/**
+ * main - Copy the content of a file to another file.
+ * @argc: The number of command line arguments.
+ * @argv: An array of command line argument strings.
+ *
+ * Return: 0 on success, or the corresponding error code.
+ */
+int main(int argc, char *argv[])
+{
+	int fd_from, fd_to, on_close;
+	ssize_t bytes_written, bytes_read;
+	char buffer[BUFFER_SIZE];
+	char *file_from, *file_to;
+
+	if (argc != 3)
+	{
+		print_error_and_exit("Usage: cp file_from file_to\n", NULL, 97);
+	}
+
+	file_from = argv[1];
+	file_to = argv[2];
+
+	fd_to = open_file(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	fd_from = open_file(file_from, O_RDONLY, 0);
+
+	while (1)
+	{
+		bytes_read = read(fd_from, buffer, BUFFER_SIZE);
+		if (bytes_read == -1)
+			print_error_and_exit(READ_ERR, file_from, 98);
+
+		if (bytes_read > 0)
+		{
+			bytes_written = write(fd_to, buffer, bytes_read);
+			if (bytes_written == -1)
+				print_error_and_exit(WRITE_ERR, file_to, 99);
+		} else
+			break;
+	}
+	close_file(fd_from);
+	close_file(fd_to);
+
+	return (0);
 }

35a3c885657eb7416c681cdfa292a7647e557122
Yousif Abozid <yousif.abozid@yahoo.com>
2023-08-21 15:57:10 +0300

    add 0x18-dynamic_libraries task 3

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/YousifAbozid/alx-low_level_programming/raw/master/0x18-dynamic_libraries/gm_crack.so
+wget -P /tmp https://github.com/YousifAbozid/alx-low_level_programming/raw/source/0x18-dynamic_libraries/gm_crack.so
 export LD_PRELOAD=/tmp/gm_crack.so
